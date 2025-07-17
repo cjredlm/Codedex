55ce74ee8642fdf5cc4947068aa895cb52afbe11
Carlos Rua <69258123+cjredlm@users.noreply.github.com>
2025-07-17 13:42:54 -0600

    Create bday_messages.py

---
+++ bday_messages.py
@@ -0,0 +1,13 @@
+import random
+
+
+bday_messages = ['Hope you have a very Happy Birthday! 🎈', 
+                 'Its your special day – get out there and celebrate! 🎉', 
+                 'You were born and the world got better – everybody wins! 🥳',
+                 'Have lots of fun on your special day! 🎂', 
+                 'Another year of you going around the sun! 🌞']
+
+
+random_message = random.choices(bday_messages)
+
+print(random_message)
