5de0358d3ce8af8a8611b31ab144906cdd72f8d8
cjredlm <cjredlm@gmail.com>
2025-07-18 22:51:36 -0600

    Your commit message here!

--- sent_message1.py
+++ sent_message1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 sent_message = 'Hola mundo Este soy yo'
 
@@ -25,32 +24,3 @@ with open('sent_message', 'r+') as file:  # r+ permite leer y escribir sin trunc
 
 # Mostrar los mensajes
 print('Original Message:', original_message)
-=======
-
-sent_message = 'Hola mundo Este soy yo'
-
-# Escribir el mensaje original en el archivo
-with open('sent_message', 'w') as file:
-    file.write(sent_message)
-
-# Leer el mensaje original y modificar el archivo en el mismo bloque
-with open('sent_message', 'r+') as file:  # r+ permite leer y escribir sin truncar
-    # Leer el mensaje original
-    original_message = file.read()
-    
-    # Mover el cursor al inicio o donde quieras escribir
-    file.seek(0)
-    
-    # Mensaje modificado
-    unsent_message = 'Este mensaje no ha sido enviado.'
-    
-    # Truncar el archivo al nuevo tamaño
-    file.truncate(len(unsent_message))
-    
-    # Escribir el mensaje modificado
-    file.write(unsent_message)
-
-# Mostrar los mensajes
-print('Original Message:', original_message)
->>>>>>> 578aa10 (Primer commit)
-print('Unsent Message:', unsent_message)
