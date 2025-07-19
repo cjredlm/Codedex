0e48e4edc85f7adcb0f04c66a5afe9e057c3d2d2
cjredlm <cjredlm@gmail.com>
2025-07-18 22:01:06 -0600

    Your commit message here!

--- diario.py
+++ diario.py
@@ -1,14 +1,14 @@
-<<<<<<< HEAD
+
 
 with open('diaries.txt', 'w') as file:
   file.write('Date: March 19, 2024 \n')
   file.write('Dear Diary...')
 
-=======
+
 
 with open('diaries.txt', 'w') as file:
   file.write('Date: March 19, 2024 \n')
   file.write('Dear Diary...')
 
->>>>>>> 578aa10 (Primer commit)
+
   file = open()
